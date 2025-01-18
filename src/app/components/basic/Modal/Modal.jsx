1acc8b4b7e31b1c549b509c5507f99ba2919245d
임승진 <seungjin051@gmail.com>
2025-01-19 01:38:02 +0900

    feature/mypage (#4)

--- src/app/components/basic/Modal/Modal.jsx
+++ src/app/components/basic/Modal/Modal.jsx
@@ -1,23 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./index.module.css";
-export default function Modal({ type }) {
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './index.module.css';
+import { locations } from '../GoogleMap/constants';
+
+export default function Modal({ type, onClose }) {
+  const location = locations.find(loc => loc.type === type);
+
   return (
-    <div className={styles.modalContainer}>
-      {/* 배경 이미지 */}
-      <Image
-        src="/modalBackground.png"
-        alt="modal"
-        width={260}
-        height={260}
-        className={styles.modalBackground}
-      />
-
-      {/* 텍스트 */}
-      <p className={styles.modalText}>
-        You got level 12! Take your reward, hero!
-      </p>
-
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modalContainer} onClick={e => e.stopPropagation()}>
+        <Image
+          src="/modalBackground.png"
+          alt="modal background"
+          width={260}
+          height={260}
+          className={styles.modalBackground}
       {type === "chest" ? (
         // 상자(chest) 이미지
         <Image
@@ -36,13 +33,43 @@ export default function Modal({ type }) {
           height={60}
           className={styles.Icon}
         />
-      )}
-
-      {/* 버튼 */}
-      <Link href="/" className={styles.buttonLink}>
-        <Image src="/button.svg" alt="button" width={70} height={70} />
-        <p className={styles.buttonText}>Open!</p>
-      </Link>
+        <div className={styles.randMarkText}>{location.address}</div>
+        <div className={styles.modalTitle}>{location.name}</div>
+        <div className={styles.modalText}>
+          {type === 'box' ? (
+            <div>
+              상자를 찾았어요! <br /> 열어보고 보상을 받으세요
+            </div>
+          ) : (
+            <div>
+              물음표를 찾았어요! <br /> 열어보고 보상을 받으세요
+            </div>
+          )}
+        </div>
+        {/* 마커의 타입에 따른 아이콘 표시 */}
+        {type === 'box' ? (
+          <Image
+            src="/chest.svg"
+            alt="chest"
+            width={120}
+            height={120}
+            className={styles.chestIcon}
+          />
+        ) : (
+          <Image
+            src="/question.svg"
+            alt="question"
+            width={120}
+            height={120}
+            className={styles.chestIcon}
+          />
+        )}
+        {/* 버튼 */}
+        <Link href="/" className={styles.buttonLink}>
+          <Image src="/button.svg" alt="button" width={115} height={35} />
+          <p className={styles.buttonText}>열기!</p>
+        </Link>
+      </div>
     </div>
   );
 }
